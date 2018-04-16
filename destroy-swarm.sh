e09fb1390498fe962106b995b8891ef9bca4b0e4
Mark Thebault <mark.thebault@cloudreach.com>
2018-04-16 14:27:33 +0200

    Spark scripts

---
+++ destroy-swarm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker-machine rm -f manager-1 worker-{1..3}
+# docker-machine rm -f manager-{1..3} worker-{1..3}
---
+++ stop-swarm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+docker-machine stop  manager-1 worker-{1..3}
+# docker-machine stop manager-{1..3} worker-{1..3}
